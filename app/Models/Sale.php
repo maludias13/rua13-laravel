23b9cbab7e532ab42d1c66972b97d2c3cd0dd33b
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-04 16:02:42 -0300

    feat: cria model de sale

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Sale extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'buyer_id',
+        'seller_id',
+        'quantity',
+        'unit_price',
+        'total_price',
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function buyer(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'buyer_id');
+    }
+
+    public function seller(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'seller_id');
+    }
 }
