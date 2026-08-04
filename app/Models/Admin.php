4f7bfb63a762bf75f735aa3691fdc04a780dbe26
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-04 16:04:59 -0300

    feat: cria model para o administrados

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Admin extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'email', 'password', 'cpf', 'photo', 'phone', 'birth_date',
+        'cep', 'number', 'street', 'neighborhood', 'city', 'state', 'complement',
+        'created_by',
+    ];
+
+    protected $hidden = ['password'];
+
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(Admin::class, 'created_by');
+    }
+
+    public function createdAdmins(): HasMany
+    {
+        return $this->hasMany(Admin::class, 'created_by');
+    }
 }
