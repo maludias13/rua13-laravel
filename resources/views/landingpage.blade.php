7e46afba687693ace88a93c432800d8a422535dd
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-07 16:45:45 -0300

    feat: Cria parte de promoção

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -33,6 +33,10 @@
             </div>
             </div>
         </div>
-        
+        <div class="promocao">
+            <h3>PROMO! PROMO! PROMO!</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat eos harum officiis, dolores expedita saepe, velit architecto eveniet animi dolor laborum non debitis accusamus voluptatibus, autem repellendus nesciunt excepturi obcaecati?</p>
+            <button>use o codigo "codelegal40" para obter desconto</button>
+        </div>
 </body>
 </html>
