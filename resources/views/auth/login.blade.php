0d95f537b0a9c42e99c33bb69e5c38572f0f5b4c
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-07 12:22:21 -0300

    fix: altero a dispozição dos botões de entrar e link de criar conta

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,24 +24,22 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+       
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex flex-col items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
+        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+            {{ __('Forgot your password?') }}
+        </a>
             @endif
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
+        <x-primary-button class="ms-3">
+        {{ __('ENTRAR') }}
+        </x-primary-button>
+    </div>
+
+    <div class="mt-4 text-center text-sm text-gray-600">
+    NÃO POSSUI CONTA? <a href="{{ route('register') }}" class="underline font-medium">CRIAR CONTA</a>
+    </div>
     </form>
 </x-guest-layout>
