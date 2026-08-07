6b6f3032058d6369d0991ec6151d5c434c73e1c5
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-07 12:13:42 -0300

    feat: estilizo os inputs de email e senha

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,14 +7,14 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')"   />
+            <x-input-label for="email" :value="__('EMAIL')"   />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="seu@email.com" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('SENHA')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'w-full bg-[#E6E7EB] rounded-lg px-2 py-3 focus:border-none focus:ring-0 font-roboto-slab']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'w-full bg-[#E6E7EB] border-0 rounded-lg px-2 py-3 focus:ring-0 font-roboto-slab']) }}>
