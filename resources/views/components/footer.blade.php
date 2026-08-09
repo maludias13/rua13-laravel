1d2c362163bbda1811d6e84b2a7366df13a046f6
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-09 13:53:01 -0300

    feat: adiciona links da pagina no footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,11 @@
+
+<footer class="footer">
+       <nav class="footer-links">
+        <a href="{{ route('landingpage') }}">.Home</a>
+        <a href="#">.Lançamentos</a>
+        <a href="#">.Promoções</a>
+        <a href="#">.Sobre</a>
+        <a href="#">.Coleções</a>
+        <a href="#">.Contato</a>
+    </nav>
+</footer>
