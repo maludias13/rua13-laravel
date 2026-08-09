7803a3a46d003ab87742b2b56cd6fc2b13b35c9a
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-09 12:34:04 -0300

    feat: adiciona logo na navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,8 +4,8 @@
         <a href="#">Masculino</a>
         <a href="#">Feminino</a>
         <a href="#">Acessórios</a>
-
-
-
     </nav>
+    <a href="{{ route('landingpage')}}">
+        <img src="{{ asset('public/logo-rua13branca.png" alt="logo-branca">
+    </a>
 </header>
