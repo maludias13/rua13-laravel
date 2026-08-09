573cb2aa2366afc2fb226fc3ec4e38accf429cc4
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-09 12:31:33 -0300

    feat: adiciona links na navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,11 @@
+<header class="navbar">
+    <nav class="navbar-nav">
+        <a href="#">Lançamentos</a>
+        <a href="#">Masculino</a>
+        <a href="#">Feminino</a>
+        <a href="#">Acessórios</a>
+
+
+
+    </nav>
+</header>
