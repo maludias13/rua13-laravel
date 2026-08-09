90f7084e7413a1e5cfac60f22d6f2454bf511afe
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-09 12:58:58 -0300

    feat: adiciona navbar na landing page

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,5 @@
-<header class="navbar">
+@props(['variant' => 'preta'])
+<header class="navbar navbar-{{ $variant }}">
     <nav class="navbar-nav">
         <a href="#">Lançamentos</a>
         <a href="#">Masculino</a>
@@ -6,11 +7,11 @@
         <a href="#">Acessórios</a>
     </nav>
     <a href="{{ route('landingpage')}}">
-        <img src="{{ asset('public/logo-rua13branca.png" alt="logo-branca">
+        <img src="{{ asset('media/logo-rua13branca.png')}}" alt="logo-branca">
     </a>
     <div class="icons-navb">
-        <img src="{{ asset('public/Heartblack.svg') }}" alt="coracao">
-        <img src="{{ asset('public/peoleblack.svg') }}" alt="pessoa">
-        <img src="{{ asset('public/CartCheckblack.svg') }}" alt="carrinho">
+        <img src="{{ asset('media/Heartblack.svg') }}" alt="coracao">
+        <img src="{{ asset('media/peoleblack.svg') }}" alt="pessoa">
+        <img src="{{ asset('media/CartCheckblack.svg') }}" alt="carrinho">
     </div>
 </header>
