855a1471a0b09e60af0794ef0b243c0767288d63
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-10 11:03:09 -0300

    feat: Adiciona relacionamento entre produto e tamanho do produto

--- database/migrations/2026_08_10_135725_create_product_size_table.php
+++ database/migrations/2026_08_10_135725_create_product_size_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('product_size', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+        $table->foreignId('size_id')->constrained()->cascadeOnDelete();
         });
     }
 
