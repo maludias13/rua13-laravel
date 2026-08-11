b88770faabb7377d6a645d18b3cca17f7d587845
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-10 23:26:51 -0300

    feat: adiciona estilização no container de produtos

--- resources/views/produto.blade.php
+++ resources/views/produto.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página de Produto</title>
+    <title>{{$product->name}}-Rua13</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <x-navbar variant="preta"/>
