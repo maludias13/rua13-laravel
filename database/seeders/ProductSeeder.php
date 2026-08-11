36d0d0a2db3a79b9326f9104eb4ebde56e3ed962
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-11 11:43:49 -0300

    feat: adiciona as tres fotos no produto individual

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -18,7 +18,7 @@ class ProductSeeder extends Seeder
         $allSizes = Size::pluck('id');
 
         $products = [
-            ['name' => 'Casaco rchangel', 'price' => 140, 'category_id' => $masculino, 'photo' => 'casaco-rchangel.jpg'],
+            ['name' => 'Camisa Polo', 'price' => 100, 'category_id' => $masculino, 'photos' => ['camisapolof.png', 'camisapoloc.png', 'camisapologola.png']],
             ['name' => 'Conjunto de colares', 'price' => 30, 'category_id' => $acessorios, 'photo' => 'conjunto-de-colares.jpg'],
             ['name' => 'Camisa Oversized', 'price' => 89, 'category_id' => $masculino, 'photo' => 'camisa-oversized.jpg'],
             ['name' => 'Óculos Punk', 'price' => 65, 'category_id' => $acessorios, 'photo' => 'oculos-punk.jpg'],
