4ea308a97b791db66a1331ee043923459425d971
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-12 20:10:29 -0300

    feat: adiciona controller de vendas

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class SaleController extends Controller
 {
-    //
+    public function index()
+    {
+        if (is_current_user_admin()) {
+            $sales = Sale::latest()->get();
+        } else {
+            $sales = Sale::where('seller_id', auth()->id())->latest()->get();
+        }
+
+        return view('vendas.index', ['sales' => $sales]);
+    }
 }
