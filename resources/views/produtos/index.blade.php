73b3ed1b48227ad217a6279c70acbc1b37dde8a2
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-12 14:36:37 -0300

    cria pagina de gerenciamento de produtos

---
+++ resources/views/produtos/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciar Produtos</title>
+</head>
+<body>
+    <div class="sidebar">
+        <div class="geren-prod-container">
+            <h2><img src="" alt="">Gerenciamento de Produtos</h2>
+            <div class="busca-prod">
+                <input type="text" placeholder="Buscar por nome, categoria ou autor">
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
