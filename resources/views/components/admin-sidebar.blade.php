0b4b8ab41a900cfdec440d39b952a57a69d8e05d
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-12 20:04:28 -0300

    feat: adiciona sidebar com links para as paginas da dashboard

---
+++ resources/views/components/admin-sidebar.blade.php
@@ -0,0 +1,15 @@
+<div class="adminsidebar">
+    <img src="{{ asset('logo-rua13branca.png') }}" alt="logo">
+    <h4>MENU</h4>
+    <h4>DASHBOARD</h4>
+    <h4>MEU PERFIL</h4>
+    <div class="telas">
+        <h4>ADMINISTRAÇÃO</h4>
+        <a href="#">Gerenciamento de Admin</a>
+        <a href="#">Gerenciamento de Usuários</a>
+        <a href="#">Gerenciamento de Produtos</a>
+        <a href="#">Historico de Vendas</a>
+        <a href="#">Gráfico de Produtos</a>
+        <a href="#">Gráfico de Vendas</a>
+    </div>
+</div>
