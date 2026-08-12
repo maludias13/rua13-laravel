ca834163ebe6b4cd51d06264e27b1a2d8156dec5
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-11 22:52:28 -0300

    feat: adiciona botão para salvar edição

--- resources/views/produtos/edit.blade.php
+++ resources/views/produtos/edit.blade.php
@@ -48,6 +48,7 @@
             @error('photo')
                 <span class="erro">{{ $message }}</span>
             @enderror
+            <button type="submit">Salvar alterações</button>
         </form>
     </div>
 </body>
