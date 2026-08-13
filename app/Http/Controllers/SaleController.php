b1c640ad6d5cbb25e174b29b7466dd602a06d394
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-12 21:02:06 -0300

    feat: adiciona migartion para tamanho na tela de vendas

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -7,13 +7,22 @@ use App\Models\Sale;
 
 class SaleController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        if (is_current_user_admin()) {
-            $sales = Sale::latest()->get();
-        } else {
-            $sales = Sale::where('seller_id', auth()->id())->latest()->get();
-        }
+        $search = $request->query('search');
+
+        $query = is_current_user_admin()
+            ? Sale::query()
+            : Sale::where('seller_id', auth()->id());
+
+        $sales = $query
+            ->when($search, function ($q) use ($search) {
+                $q->whereHas('product', function ($productQuery) use ($search) {
+                    $productQuery->where('name', 'like', "%{$search}%");
+                });
+            })
+            ->latest()
+            ->get();
 
         return view('vendas.index', ['sales' => $sales]);
     }
--- resources/views/vendas/index.blade.php
+++ resources/views/vendas/index.blade.php
@@ -15,7 +15,25 @@
     </div>
     <div class="prodvendido">
         @foreach($sales as $sale)
-        <div class=""></div>
+        @if($sale->product)
+        <img src="{{ asset('media/' . $sale->product->photo) }}" alt="{{ $sale->product->name }}" width="40">
+        @endif
+        <div class="conteudo-venda">
+            <div class="np-venda">
+                <h3>{{$sale->product->name}}</h3>
+                <h3>{{format_price($sale->total_price)}}</h3>
+            </div>
+            <div class="tamdata-venda">
+                <h3>TAMANHO {{$product->size}}</h3>
+                <p>Vendido em {{ $sale->created_at->format('d/m/Y') }}</p>
+            </div>
+            @if ($sale->product)
+                <a href="{{ route('produto.show', $sale->product->id) }}">Ver Produto</a>
+            @endif
+            @empty
+            <p>Nenhuma venda encontrada.</p>
+            @endforelse
+        </div>
         @endforeach
     </div>
 </body>
