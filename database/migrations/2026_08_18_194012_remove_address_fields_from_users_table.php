cb25c74e6818b66399f15099ed103ccdf87eb9fa
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-18 16:44:20 -0300

    feat: adiciona e ajusta tabela de excluir endereço

--- database/migrations/2026_08_18_194012_remove_address_fields_from_users_table.php
+++ database/migrations/2026_08_18_194012_remove_address_fields_from_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+        $table->dropColumn(['cep', 'number', 'street', 'neighborhood', 'city', 'state', 'complement']);
         });
     }
 
@@ -22,7 +22,13 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+        $table->string('cep')->nullable();
+        $table->string('number')->nullable();
+        $table->string('street')->nullable();
+        $table->string('neighborhood')->nullable();
+        $table->string('city')->nullable();
+        $table->string('state')->nullable();
+        $table->string('complement')->nullable();
         });
     }
 };
