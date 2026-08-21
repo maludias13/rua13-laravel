231bc0dc767f67c8137603dcf1847af5095d937a
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-08-21 16:37:10 -0300

    feat: alinha itens so centro da pagina

--- resources/views/profile/perfil.blade.php
+++ resources/views/profile/perfil.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Meu Perfil - Rua 13</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/profile.css', 'resources/js/app.js'])
 </head>
 <body>
 
